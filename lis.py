2de861506360ec0d5cda4a537937a98a64f9ddc3
udayshankar <udayshankarlella@gmail.com>
2017-12-08 18:07:24 +0530

    first program in python

---
+++ lis.py
@@ -0,0 +1,18 @@
+x = 10
+print("x value is ",x)
+print(type(x) is int)
+print(type(x) is float)
+print(type(x) is str)
+
+x = "python"
+print("x value is ",x)
+print(type(x) is not int)
+print(type(x) is not float)
+print(type(x) is not str)
+print(type(x) is str)
+
+x = 'a'
+y = 'raja'
+z = 'b'
+print(x in y)
+print(z not in y)
